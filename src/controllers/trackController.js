bd93f9a7c1e23dfbbbb2da20be8ef0940aa4aaaf
Ali Asghar <aldigod123@gmail.com>
2024-04-30 14:09:05 +0300

    Added transactions for data integrity

--- src/controllers/trackController.js
+++ src/controllers/trackController.js
@@ -2,10 +2,10 @@ const pool = require('../database');
 
 const getAllTracks = async (req, res) => {
     try {
-        const client = await pool.connect();
-        const result = await client.query('SELECT * FROM tracks');
+        //const client = await pool.connect();
+        const result = await pool.query('SELECT * FROM tracks');
         res.json(result.rows);
-        client.release();
+        //client.release();
     } catch (err) {
         console.error('Error executing query', err);
         res.status(500).send('Internal Server Error');
@@ -15,14 +15,14 @@ const getAllTracks = async (req, res) => {
 const getTrackById = async (req, res) => {
     const id = parseInt(req.params.id);
     try{
-        const client = await pool.connect();
-        const result = await client.query('SELECT * FROM tracks WHERE id = $1', [id]);
+        //const client = await pool.connect();
+        const result = await pool.query('SELECT * FROM tracks WHERE id = $1', [id]);
         if (result.rows.length > 0) {
             res.json(result.rows[0]);
         } else {
             res.status(404).send('Track not found');
         }
-        client.release();
+        //client.release();
     } catch (err) {
         console.error('Error executing query', err);
         res.status(500).send('Internal Server Error');
@@ -31,49 +31,65 @@ const getTrackById = async (req, res) => {
 
 const createTrack = async (req, res) => {
     const {title, artist, album} = req.body;
+
+    const client = await pool.connect();
     try {
-        const client = await pool.connect();
-        const result = await client.query('INSERT INTO tracks (title, artist, album) VALUES ($1, $2, $3) RETURNING *', [title, artist, album]);
+        await client.query('BEGIN');
+        const queryText = 'INSERT INTO tracks (title, artist, album) VALUES ($1, $2, $3) RETURNING *';
+        const result = await client.query(queryText, [title, artist, album]);
         res.status(201).json(result.rows[0]);
-        client.release();
+        await client.query('COMMIT');
     } catch (err) {
         console.error('Error executing query', err);
         res.status(500).send('Internal Server Error');
+        await client.query('ROLLBACK');
+    } finally {
+        client.release();
     }
 }
 
 const updateTrack = async (req, res) => {
     const id = parseInt(req.params.id);
     const {title, artist, album} = req.body;
+    const client = await pool.connect();
     try {
-        const client = await pool.connect();
-        const result = await client.query("UPDATE tracks SET title = $1, artist = $2, album = $3 WHERE id = $4 RETURNING *", [title, artist, album, id]);
+        await client.query('BEGIN');
+        const queryText = 'UPDATE tracks SET title = $1, artist = $2, album = $3 WHERE id = $4 RETURNING *';
+        const result = await client.query(queryText, [title, artist, album, id]);
         if (result.rows.length > 0) {
             res.json(result.rows[0]);
         } else {
             res.status(404).send('Track not found');
         }
-        client.release();
+        await client.query('COMMIT');
     } catch (err) {
         console.error('Error executing query', err);
         res.status(500).send('Internal Server Error');
+        await client.query('ROLLBACK');
+    } finally {
+        client.release();
     }
 }
 
 const deleteTrack = async (req, res) => {
     const id = parseInt(req.params.id);
+    const client = await pool.connect();
     try {
-        const client = await pool.connect();
-        const result = await client.query("DELETE FROM tracks WHERE id = $1", [id]);
+        await client.query('BEGIN');
+        const queryText = 'DELETE FROM tracks WHERE id = $1';
+        const result = await client.query(queryText, [id]);
         if (result.rowCount > 0) {
             res.status(204).send('Track deleted successfully');
         } else {
             res.status(404).send('Track not found');
         }
-        client.release();
+        await client.query('COMMIT');
     } catch(err) {
         console.error('Error executing query', err);
         res.status(500).send('Internal Server Error');
+        await client.query('ROLLBACK');
+    } finally {
+        client.release();
     }
 }
 
